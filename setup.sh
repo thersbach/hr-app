4bf33355ed35fd1ef097d3f4f15362e34798cc71
Terence Hersbach <git@terencehersbach.nl>
2024-09-04 13:06:10 +0200

    Updates setup script to listen to the container before making changes

--- setup.sh
+++ setup.sh
@@ -4,10 +4,17 @@
 # Start docker-compose
 docker-compose up -d
 
-# wait till there is a connection possible to port 3306
-while ! nc -z localhost 3306; do
+# wait till there is a connection possible to port 3306 and it is possible to get a status from mariadb
+while ! nc -z 127.0.0.1 3306; do
   sleep 0.1
 done
+
+while ! docker exec -i mariadb mariadb -u root -prootpassword -e "status"; do
+  sleep 5
+done
+
+# install composer
+composer install
 
 # Start symfony
 php bin/console doctrine:migrations:migrate -n
